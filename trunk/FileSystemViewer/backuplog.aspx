51deba576590362e144d073003b5ab623a4b509d
jomora <jomora@6e208f6c-714a-9f45-812f-5c65dd2b05be>
2012-06-29 06:22:55 +0000

    バックアップログをWeb(RSS)で閲覧するためのページを追加

---
+++ trunk/FileSystemViewer/backuplog.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="backuplog.aspx.cs" Inherits="backuplog" %>
+<%
+    if ("rss" == Request.Params["format"])
+    {
+	    // RSS
+	    Response.ContentType = "text/xml; charset=UTF-8";
+        string linkUrl = Request.Url.ToString().Substring(0, Request.Url.ToString().Length - Request.Url.Query.Length);
+%><?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+   <channel>
+      <title>backup archive</title>
+      <link><%= linkUrl%></link>
+      <description>
+<%
+	    // 表示を省略したログのカウント
+	    foreach (System.Collections.Generic.KeyValuePair<string, int> omitted in omittedLogs)
+        {
+            Response.Write(string.Format("{0, 5}", omitted.Value) + " " + HttpUtility.HtmlEncode(omitted.Key) + "<br />\n");
+	    }
+%>      </description>
+<%
+        System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-US");
+	    foreach (string log in logs)
+        {
+		    string title = log.Substring(20);
+            string pubDate = DateTime.Parse(log.Substring(0, 18)).ToString("ddd, dd MMM yyyy HH:mm:ss zzz", ci);
+            string guid = DateTime.Parse(log.Substring(0, 18)).ToString("yyyy-MM-ddTHH:mm:sszzz", ci);
+%>      <item>
+         <title><%= HttpUtility.HtmlEncode(title) %></title>
+         <link><%= HttpUtility.HtmlEncode(linkUrl) %></link>
+         <pubDate><%= pubDate %></pubDate>
+         <guid isPermaLink="false"><%= guid %></guid>
+      </item>
+<%
+        }
+%>   </channel>
+</rss>
+<%
+    } else {
+        // TEXT
+        Response.ContentType = "text/plain; charset=UTF-8";
+        // 表示を省略したログのカウント
+        foreach (System.Collections.Generic.KeyValuePair<string, int> omitted in omittedLogs)
+        {
+            Response.Write(string.Format("{0, 5}", omitted.Value) + " " + omitted.Key + "\n");
+        }
+        Response.Write("\n");
+        foreach (string log in logs)
+        {
+            Response.Write(log + "\n");
+        }
+    }
+%>
---
+++ trunk/FileSystemViewer/backuplog.aspx.cs
@@ -0,0 +1,81 @@
+﻿//
+// Backup archive Log Viewer
+//
+// @author Jomora ( kazuhiko@jomura.net http://jomura.net/ )
+// @version 2012.06.29 Jomora created
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+
+public partial class backuplog : System.Web.UI.Page
+{
+    // ログファイルの指定
+    private string logFile = "E:/@backup/backupArchive.log";
+
+    // 表示を省略するログ
+    protected Dictionary<string, int> omittedLogs = new Dictionary<string, int>();
+
+    // 最大行数
+    private int maxLineSize = 500;
+    // 1行の最大文字数
+    private int maxLength = 4096;
+
+    // 出力するログ
+    protected List<string> logs = new List<string>();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        omittedLogs.Add(" [notice] ", 0);
+
+        // ファイルの読み込み
+        string[] allLines = File.ReadAllLines(logFile, Encoding.GetEncoding("Shift_JIS"));
+
+        // 表示を省略するかどうか判定して、logsに追加
+        foreach (string buffer in allLines)
+        {
+            if (string.IsNullOrEmpty(buffer)) continue;
+            bool canView = true;
+            foreach (string omittedKey in omittedLogs.Keys)
+            {
+                if (canView && -1 != buffer.IndexOf(omittedKey))
+                {
+                    omittedLogs[omittedKey]++;
+                    canView = false;
+                }
+            }
+
+            if (canView)
+            {
+                if (buffer.Length > maxLength)
+                {
+                    logs.Add(buffer.Remove(maxLength));
+                }
+                else
+                {
+                    logs.Add(buffer);
+                }
+            }
+        }
+
+        // 最新のログから最大表示行数分だけ表示する
+        if (maxLineSize < logs.Count) 
+        {
+            logs.RemoveRange(0, logs.Count - maxLineSize);
+        }
+
+        // 新しいものから表示
+        logs.Reverse();
+
+        // for debug
+        for (int i = 0; i < logs.Count; i++)
+        {
+            Debug.WriteLine(i + " : " + logs[i]);
+        }
+    }
+}
